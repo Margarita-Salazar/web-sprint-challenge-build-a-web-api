bf2700af6c888dda16563249dc3d51368b33cd74
unknown <margarita.salazar.mls@gmail.com>
2021-06-04 11:06:54 -0500

    wip

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,20 @@
 // Write your "actions" router here!
+const router = require('express').Router()
+
+router.get('/', (req, res)=>{
+    
+})
+router.get('/', (req, res)=>{
+    
+})
+router.post('/', (req, res)=>{
+    
+})
+router.put('/', (req, res)=>{
+    
+})
+router.delete('/', (req, res)=>{
+    
+})
+
+module.exports = router
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,24 @@
 // Write your "projects" router here!
+
+const router = require('express').Router()
+
+router.get('/', (req, res)=>{
+    
+})
+router.get('/', (req, res)=>{
+    
+})
+router.post('/', (req, res)=>{
+    
+})
+router.put('/', (req, res)=>{
+    
+})
+router.delete('/', (req, res)=>{
+    
+})
+router.get('/', (req, res)=>{
+    
+})
+
+module.exports = router
